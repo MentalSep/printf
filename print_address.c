e83b78bc029fde84ee9dc4a8de85f1880803c443
MentalSeppuku <chahidjava@hotmail.com>
2023-07-22 20:17:46 +0100

    fix print_address in case NULL is passed

--- print_address.c
+++ print_address.c
@@ -8,6 +8,11 @@ void print_address(va_list args, int *count)
 {
 	unsigned long int address = (unsigned long int)va_arg(args, void *);
 
+	if (!address)
+	{
+		_puts("(nil)", count);
+		return;
+	}
 	_puts("0x", count);
 	hex_helper(address, "0123456789abcdef", count);
 }
